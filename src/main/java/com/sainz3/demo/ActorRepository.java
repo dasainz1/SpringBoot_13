1e09cb3c39e72d37cc59f87e09acffa539e0098a
danielasainz <dasainz@email.wm.edu>
2018-03-21 17:06:32 -0400

    Initial commit - will likely be missing HomeController.java

---
+++ src/main/java/com/sainz3/demo/ActorRepository.java
@@ -0,0 +1,5 @@
+package com.sainz3.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ActorRepository extends CrudRepository<Actor, Long> {}
---
+++ src/main/java/com/sainz3/demo/MovieRepository.java
@@ -0,0 +1,5 @@
+package com.sainz3.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MovieRepository extends CrudRepository<Movie, Long>{}
