104b207d59a7deb099ca1ad3bd5d5921c6c76bd6
danielasainz <dasainz@email.wm.edu>
2018-03-22 15:12:34 -0400

    fixed functionalities

--- src/main/java/com/sainz3/demo/HomeController.java
+++ src/main/java/com/sainz3/demo/HomeController.java
@@ -5,35 +5,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import java.util.HashSet;
 import java.util.Set;
 
 @Controller
 public class HomeController {
 
+
     @Autowired
     ActorRepository actorRepository;
 
     @Autowired
-    MovieRepository movieRepository
+    MovieRepository movieRepository;
 
     @RequestMapping("/")
-    public String loadData(){
+    public String loadData(Model model) {
 
-    Actor a = new Actor();
-    a.setName("Sandra Bullock");
-    a.setRealname("Sandra Mae Bullowski");
-    actorRepository.save(a);
+        Actor a = new Actor();
+        a.setName("Sandra Bullock");
+        a.setRealname("Sandra Mae Bullowski");
+        actorRepository.save(a);
 
-    Movie movie = new Movie();
-    movie.setTitle("Emoji Movie");
-    movie.setYear(2017);
-    movie.setDescription("About Emojis...");
+        Movie movie = new Movie();
+        movie.setTitle("Emoji Movie");
+        movie.setYear(2017);
+        movie.setDescription("About Emojis...");
+        movieRepository.save(movie);
 
-    actor.addMovie(movie);
-    actorRepository.save(actor);
+        a.addMovie(movie);
+        actorRepository.save(a);
 
-    model.addAttribute("actors", actorRepository.findAll());
-    return "index";
+       model.addAttribute("actors", actorRepository.findAll());
+       return "index";
+       
     }
 }
